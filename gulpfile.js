034d91c730136ffe727ff46bdebbe68bdaa9fe51
rymopl <lukasz.riemer@gmail.com>
2018-02-20 23:19:28 +0100

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp'),
     concatJs = require('gulp-concat'),
     sass = require('gulp-sass'),
-   
-    path = require('path');
-    
+    cleanCSS = require('gulp-clean-css');    
 
 
     gulp.task('sass', function () {
         return gulp.src('assets/sass/main.scss')
             .pipe(sass().on('error', sass.logError))
             .pipe(concatJs('apploads.css'))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest('css/'));
     });
     
